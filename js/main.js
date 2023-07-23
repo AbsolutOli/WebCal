a435279f0fb5614459fc20bb5edf01a4be8d4a65
AbsolutOli <vocameoli@gmail.com>
2023-07-23 14:02:36 +0300

    Add save solutions in history

--- js/main.js
+++ js/main.js
@@ -2,24 +2,37 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
+let historyItemOperation = null;
+let historyItemSolution = "= ";
 
-const screen = document.querySelector('.calc__screen')
-
+const screen = document.querySelector('.calc__screen');
+const historyList = document.querySelector('.history__list');
 
 function buttonClick(value) {
     if (isNaN(value) || value === "") {
-        console.log(`some sym ${value}`);
         handleSymbol(value);
 
     } else {
-        console.log(`some num ${value}`);
         handleNumber(value);
     }
-    console.log(`runningTotal = ${runningTotal} + ${typeof (buffer)} \n buffer = ${buffer} \n previousOperator = ${previousOperator}`);
+    console.log(`historyItemOperation = ${historyItemOperation} \n historyItemSolution = ${historyItemSolution}`);
 
     screen.innerText = buffer;
 
     screen.scroll(1000, 0);
+
+    if (historyItemOperation !== null && historyItemSolution !== "= ") {
+        historyList.insertAdjacentHTML('afterbegin', `<li class="history__item">
+        <div class="history__delete"></div>
+        <div class="history__content">
+            <div title="${historyItemOperation}" class="history__operaion-text">${historyItemOperation}</div>
+            <div title="${historyItemSolution}" class="history__answer-text">${historyItemSolution}</div>
+        </div>
+    </li>`)
+
+        historyItemOperation = null;
+        historyItemSolution = "= ";
+    }
 }
 
 function handleSymbol(symbol) {
@@ -27,6 +40,7 @@ function handleSymbol(symbol) {
         case 'C':
             buffer = '0';
             runningTotal = 0;
+            historyItemOperation = null;
             break;
         case '=':
             if (previousOperator === null) {
@@ -34,17 +48,20 @@ function handleSymbol(symbol) {
             }
             flushOperation(parseInt(buffer));
             previousOperator = null;
+
+            historyItemOperation += buffer;
             buffer = String(runningTotal);
+
+            historyItemSolution += runningTotal;
             runningTotal = 0;
             break;
         case '<':
             if (buffer.length === 1) {
                 buffer = '0';
+                historyItemOperation = null;
             } else {
-                console.log(buffer);
-                console.log(buffer.length);
                 buffer = buffer.substring(0, buffer.length - 1);
-                console.log(buffer);
+                historyItemOperation
             }
             break;
         case '+':
@@ -59,9 +76,15 @@ function handleSymbol(symbol) {
             if (buffer === '0') {
                 return;
             }
+            if (historyItemOperation === null) {
+                historyItemOperation = buffer;
+            } else {
+                historyItemOperation += buffer;
+            }
             handleMathWithOneNumber(symbol);
             previousOperator = null;
             buffer = String(runningTotal);
+            historyItemSolution += runningTotal;
             runningTotal = 0;
             break;
     }
@@ -70,32 +93,50 @@ function handleSymbol(symbol) {
 function handleMathWithOneNumber(symbol) {
     previousOperator = symbol;
     if (previousOperator === '%') {
+        historyItemOperation += previousOperator;
         runningTotal = parseFloat(buffer) / 100;
     }
     if (previousOperator === '√') {
+        historyItemOperation = previousOperator + historyItemOperation;
         runningTotal = Math.sqrt(parseFloat(buffer));
     }
     if (previousOperator === 'x²') {
-        runningTotal = parseInt(buffer) * parseInt(buffer);
+        historyItemOperation = historyItemOperation + '²';
+        runningTotal = parseFloat(buffer) * parseFloat(buffer);
     }
 }
 
 function handleMath(symbol) {
-    if (buffer === '0' && runningTotal === 0) {
+    if (buffer === '0' || (buffer === '0' && runningTotal === 0)) {
+
+        previousOperator = symbol;
         return;
     }
 
-    const intBuffer = parseInt(buffer);
+    const intBuffer = parseFloat(buffer);
 
     if (runningTotal === 0) {
         runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
     }
 
     previousOperator = symbol;
+
+    console.log(buffer);
+    console.log(previousOperator);
+
+    if (historyItemOperation === null) {
+        historyItemOperation = buffer;
+    } else {
+        historyItemOperation += buffer;
+    }
+
     buffer = '0';
 }
 
 function flushOperation(intBuffer) {
+    historyItemOperation += previousOperator;
     if (previousOperator === '+') {
         runningTotal += intBuffer;
     } else if (previousOperator === '−') {
@@ -117,11 +158,15 @@ function handleNumber(numberString) {
 
 function init() {
     document.querySelector('.calc__buttons').addEventListener('click', function (event) {
-        if (event.target.classList.contains('calc__side-buttons_arrow')) {
+        if (event.target.classList.contains('calc__side-buttons_arrow') || event.target.classList.contains('history__side-buttons_arrow')) {
             event.target.parentNode.classList.toggle('active');
         } else {
             buttonClick(event.target.innerText);
-            console.log(event);
+        }
+    })
+    document.querySelector('.calc__history').addEventListener('click', function (event) {
+        if (event.target.classList.contains('history__side-buttons_arrow')) {
+            event.target.parentNode.classList.toggle('active');
         }
     })
 }
